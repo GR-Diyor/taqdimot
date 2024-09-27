53500a5708588db3f202984d159eddde2698f436
GR-Diyor <nizomiddinov.d.d@gmail.com>
2024-09-27 11:17:07 +0500

    created,must have files

---
+++ lib/feature/data/data_source/entity/user_entitie/user_entitie.dart
@@ -0,0 +1,8 @@
+class UserEntitie{
+  final String id;
+  final String name;
+  final String surname;
+  final String? avatarImage;
+
+  UserEntitie({required this.id, required this.name, required this.surname, this.avatarImage});
+}
